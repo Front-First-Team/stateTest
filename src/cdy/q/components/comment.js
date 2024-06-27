2e2e6374be069a19e1f451752cc52e417c498444
Kathy Dayoon Choi <76470322+itskathyc@users.noreply.github.com>
2024-06-27 09:32:31 +0900

    Update comment.js

--- src/cdy/q/components/comment.js
+++ src/cdy/q/components/comment.js
@@ -1,41 +1,19 @@
 import styled from "styled-components";
 
-function Comment({commentlist}) {
-  
-    return (
-      {
-        commentlist.map((comment)=> {
-        <S.CommentItem>
-        <p>
-          작성자: <span>{writer}</span>
-        </p>
-        <p>
-          댓글 내용: <span>예시 내용</span>
-        </p>
-      </S.CommentItem>
-      
-      })}
+function Comment({ comment }) {
+  console.log("넘어오는 값 : ", comment);
+
+  return (
+    <S.CommentItem>
+      <p>
+        작성자: <span>{comment.User.nickname}</span>
+      </p>
+      <p>
+        댓글 내용: <span>{comment.content}</span>
+      </p>
+    </S.CommentItem>
   );
-  
-    }
-
-
-// function Comment({nickname, content}) {
-//   return (
-//     <>
-//       <S.CommentItem>
-//       <p>
-//         작성자: <span>{nickname}</span>
-//       </p>
-//       <p>
-//         댓글 내용: <span>{content}</span>
-//       </p>
-//       <button>수정</button>
-//       <button>삭제</button>
-//     </S.CommentItem>
-//     </>
-//   );
-// }
+}
 export default Comment;
 
 const CommentItem = styled.li`

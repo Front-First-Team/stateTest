99a9d726a459271be02c07a87332aef671fa9454
Kathy Dayoon Choi <76470322+itskathyc@users.noreply.github.com>
2024-07-12 16:53:45 +0900

    Update Detail.js

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,7 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 function DetailPage() {
-  const params = useParams();
-  console.log(params.productNumber);
+  // const params = useParams();
+  // console.log("파람스 : ", params);
+
+  const {
+    state: {
+      product: {
+        productPrice,
+        productName,
+        productSize,
+        productRating,
+        productReview,
+      },
+    },
+  } = useLocation();
 
   return (
     <div>
@@ -12,7 +24,18 @@ function DetailPage() {
 
       단, 없는 번호 상품으로 접근 시 state페이지로 돌아가도록 구현해주세요
      */}
-      {params.productNumber}
+      <div style={{ border: "1px solid black" }}>
+        <p>상품이름: {productName}</p>
+        <p>가격: {productPrice}원</p>
+        {/*1000단위로 , 붙이기 */}
+        <p>사이즈: {productSize}</p>
+        <p>평점: {productRating}</p>
+        <p>리뷰: {productReview}</p>
+      </div>
+      <div>
+        {/*구매평 */}
+        {/* 구매평 추가 */}
+      </div>
     </div>
   );
 }
